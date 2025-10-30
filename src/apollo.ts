c90af51850f5484578d8bd9ff023aff0fc6f471a
zelonKim <ksz18601@gmail.com>
2025-10-30 15:22:43 +0900

    wss https

--- src/apollo.ts
+++ src/apollo.ts
@@ -15,7 +15,10 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? `wss://uber-eats-backend-701bf2fe6dd6.herokuapp.com/graphql`
+      : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
@@ -25,7 +28,10 @@ const wsLink = new WebSocketLink({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? `https://uber-eats-backend-701bf2fe6dd6.herokuapp.com/graphql`
+      : `http://localhost:4000/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
