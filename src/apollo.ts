079803bfe22b58e4a2f21f40a52d18aca7e55cb6
zelonKim <ksz18601@gmail.com>
2025-12-09 11:50:28 +0900

    fix5

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,30 +3,29 @@ import {
   InMemoryCache,
   makeVar,
   createHttpLink,
-  // split, // WebSocket 비활성화로 인해 사용하지 않음
+  split, // 웹소켓 활성화
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-// import { WebSocketLink } from "@apollo/client/link/ws"; // WebSocket 비활성화
-// import { getMainDefinition } from "@apollo/client/utilities"; // WebSocket 비활성화
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
-// WebSocket 완전 비활성화 - 배포 환경에서 타임아웃 및 연결 문제 해결
-// const wsLink = new WebSocketLink({
-//   uri:
-//     process.env.NODE_ENV === "production"
-//       ? `wss://${process.env.REACT_APP_BACKEND_DEPLOY_URL}/graphql`
-//       : `ws://localhost:4000/graphql`,
-//   options: {
-//     reconnect: true,
-//     connectionParams: {
-//       "x-jwt": authTokenVar() || "",
-//     },
-//   },
-// });
+const wsLink = new WebSocketLink({
+  uri:
+    process.env.NODE_ENV === "production"
+      ? `wss://${process.env.REACT_APP_BACKEND_DEPLOY_URL}/graphql`
+      : `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      "x-jwt": authTokenVar() || "",
+    },
+  },
+});
 
 const httpLink = createHttpLink({
   uri:
@@ -44,21 +43,21 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// WebSocket 비활성화 - HTTP만 사용
-// const splitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === "OperationDefinition" &&
-//       definition.operation === "subscription"
-//     );
-//   },
-//   wsLink,
-//   authLink.concat(httpLink)
-// );
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
+  // link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
